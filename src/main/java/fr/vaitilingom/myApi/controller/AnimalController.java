47c730771c9f3fc21d72427f3f2e2778b90d1245
der411 <vaitilingomvincent@gmail.com>
2023-06-26 10:00:48 +0200

    Spring Boot Exercice 5

--- src/main/java/fr/vaitilingom/myApi/controller/AnimalController.java
+++ src/main/java/fr/vaitilingom/myApi/controller/AnimalController.java
@@ -1,6 +1,14 @@
 package fr.vaitilingom.myApi.controller;
 
+import java.net.URI;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,15 +31,57 @@ import fr.vaitilingom.myApi.service.AnimalService;
     
     @GetMapping("/animal")
     
-    public Animal getAnimal(@RequestParam int id) {
+    public ResponseEntity<?> getAnimal(@RequestParam int id) {
 
     Animal animal = animalService.getAnimal(id);
 
         if (animal != null) {
-            return animal;
+            return ResponseEntity.ok(animal);
         } else {
-            return null;
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Animal non trouvé");
         }
+    }
+
+    @PostMapping("/animal")
+
+    public ResponseEntity<?> createAnimal(@RequestBody AnimalRequest body){
+
+    Animal animal = animalService.createAnimal(body.getName(), body.getType(), body.getWeight());
+
+        if (animal != null) {
+            
+            return ResponseEntity.created(URI.create("/Animal/" + animal.getId())).body(animal);
+        
+        } else {
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Animal non créé");
+        }
+
+    }
+
+    @PutMapping("/animal") 
+
+    public ResponseEntity<?> updateAnimal(@RequestParam int id, @RequestBody AnimalRequest body){
+
+    Animal animal = animalService.updateAnimal(id, body.getName(), body.getType(), body.getWeight());
+        if (animal != null) {
+            return ResponseEntity.ok(animal);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Animal non modifié");
+        }
+    }
+
+    @DeleteMapping("/animal")
+    
+    public ResponseEntity<?> deleteAnimal(@RequestParam int id) {
+
+    Animal animal = animalService.deleteAnimal(id);
+        if (animal != null) {
+            return ResponseEntity.ok(animal);
+        } else {
+            //return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Animal non supprimé");
+    }
     }
 }
 
--- src/main/java/fr/vaitilingom/myApi/service/AnimalService.java
+++ src/main/java/fr/vaitilingom/myApi/service/AnimalService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import fr.vaitilingom.myApi.model.Animal;
 
 
+
 @Service
 public class AnimalService {
 
@@ -17,6 +18,7 @@ public class AnimalService {
         this.animals.add(new Animal(1, "Lion", "Mammifere", 200));
         this.animals.add(new Animal(2, "Requin", "Poisson", 1500));
         this.animals.add(new Animal(3, "Aigle", "Oiseau", 7));
+        this.animals.add(new Animal(4, "Cobra", "Reptile", 15));
 
     }
 
@@ -26,6 +28,35 @@ public class AnimalService {
             if (animal.getId() == id) {
         return animal;
             }
+        }
+        return null;
+    }
+
+    public Animal createAnimal(String name, String type, int weight){
+        Animal animal = new Animal(this.animals.size() + 1, name, type, weight);
+        this.animals.add(animal);
+        return animal;
+    }
+
+    public Animal updateAnimal(int id, String name, String type, int weight){
+
+        for (Animal animal : animals) {
+            if (animal.getId() == id) {
+            animal.setName(name);
+            animal.setType(type);
+            animal.setWeight(weight);
+            return animal;
+            }
+         }
+        return null;
+    }
+
+    public Animal deleteAnimal(int id){
+        for(Animal animal: this.animals){
+             if (animal.getId() == id) {
+                int index = this.animals.indexOf(animal);
+                return this.animals.remove(index);
+             }
         }
         return null;
     }
